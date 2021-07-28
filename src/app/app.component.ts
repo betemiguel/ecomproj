0b8c3a63e678a45e9ee7cbc7ea106e6d9d009aae
betemiguel <betemiguel29@gmail.com>
2021-07-28 23:25:05 +0100

    Revert "Header and footer"
    This reverts commit a8edf3ac424cd1a1f2ccbbd9adfec4a68a1fe272.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,10 @@
-
 import { Component } from '@angular/core';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
-import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
-import { faDoorClosed } from '@fortawesome/free-solid-svg-icons';
-import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-
-
 export class AppComponent {
   title = 'ecomproj';
-  faUserPlus = faUserPlus;
-  faSignInAlt = faSignInAlt ;
-  faDoorClosed = faDoorClosed;
-  faUser = faUser;
-  faShoppingBasket = faShoppingBasket;
-  
- 
- 
-
 }
-
